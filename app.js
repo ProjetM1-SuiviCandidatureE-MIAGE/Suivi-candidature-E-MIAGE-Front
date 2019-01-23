212efa8395435dd12777b2c27d2b02ce20141c10
TanguyRoue <pogoman@orange.fr>
2019-01-23 22:35:24 +0100

    Changements mineurs et rajout prise en charge CORS

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/candidature');
+mongoose.connect('mongodb://localhost/candidature').then(() => {
+    console.log('Connected to mongoDB')
+}).catch(e => {
+    console.log('Error while DB connecting');
+    console.log(e);
+});
 
 const app = express();
 
@@ -14,7 +19,16 @@ let urlencodedParser = bodyParser.urlencoded({
 app.use(urlencodedParser);
 app.use(bodyParser.json());
 
-//On définit la route Hello
+//Définition des CORS
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//Test avec une route Hello
 app.get('/hello',function(req,res){
     res.json("Hello World")
 })
