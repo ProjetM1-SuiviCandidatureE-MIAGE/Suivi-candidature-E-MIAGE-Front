3eb9b34ba0308319576d6c655805ddf63c3ab6d0
DESKTOP-54EG6FN\Jeanne Bertoux <jeannebertoux@gmail.com>
2019-01-23 09:28:23 +0100

    fonctions pour les controllers candidatures et candidats non complète

--- routes/candidats.js
+++ routes/candidats.js
@@ -3,7 +3,13 @@ let router = require('express').Router();
 let Candidat = require ('./../models/Candidat');
 
 router.get('/:candidat', (req, res) =>{
-
+    Candidat.findOne({ nom: req.params.candidat }).populate('candidatures').then(candidat => {
+        if (!candidat)return res.status(404).send('candidat introuvable');
+        res.render('chemin Damien pour les candidats', {
+            candidat: candidat,
+            candidatures: candidat.candidatures
+        });
+    }, err => console.log(err));
 });
 
 module.exports = router;
