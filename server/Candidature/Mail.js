5862916fb593e8c31a0bd4859c8618405d6ab804
TanguyRoue <pogoman@orange.fr>
2019-03-23 18:07:16 +0100

    Creation de la fonction d'envoi de mails. Operationnelle.

--- server/Candidature/Mail.js
+++ server/Candidature/Mail.js
@@ -1,39 +1,45 @@
 const nodemailer = require('nodemailer');
-const account = require('../Account/lib');
-let express = require('express').app(),
-    app = express();
+//const account = require('../Account/lib');
+const app = require('express').Router();
    
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.get('/', (req,res) =>{
+    console.log("Hi");
+});
+
 app.post('/send', (req,res) =>{
-    const output = '<p>vous avez reçu une nouvelle demande de contact</p><h3>Contact details</h3><ul><li>Name: ${req.body.name}</li><li>Email: ${req.body.email}</li></ul><h3>Message</h3><p>Email: ${req.body.message}</p>';
+  console.log("Hello");
+    //const output = "<p>vous avez reçu une nouvelle demande de contact</p><h3>Contact details</h3><ul><li>Name: ${req.body.name}</li><li>Email: ${req.body.mail}</li></ul><h3>Message</h3><p>Email: ${req.body.message}</p>";
     
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        service: "Gmail",
         auth: {
-          user: account.login, // generated ethereal user
-          pass: account.login // generated ethereal password
+          user: "gestioncandidaturem1miaa@gmail.com",
+          pass: "azertyM1MIAA"
         }
       });
     
       // setup email data with unicode symbols
       let mailOptions = {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "jeannebertoux@gmail.com", // list of receivers
-        subject: "test ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: output // html body
+        from: "gestioncandidaturem1miaa@gmail.com", // sender address
+        to: "pogoman28@gmail.com", // list of receivers
+        subject: "test", // Subject line
+        //text: "Hello world?", // plain text body
+        //html: output // html body
+        html: "Hello there"
+
       };
      
       transporter.sendMail(mailOptions, (error, info) =>{
           if(error){
+              console.log("Erreur lors de l'envoi du mail");
               return console.log(error);
           }
+          console.log("Message envoyé !");
           console.log("Message sent: %s", info.messageId);
           console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         
@@ -41,3 +47,5 @@ app.post('/send', (req,res) =>{
   
  
 });
+
+module.exports = app;
