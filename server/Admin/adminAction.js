6679ca8b1497781fe07b69696a938cab8796ffe3
DamienDnd <d.donnadieux@gmail.com>
2019-04-04 09:35:04 +0200

    suppression du dossier du back

--- server/Admin/adminAction.js
+++
@@ -1,6 +0,0 @@
-let auth = module.exports;
-const AdminProcess = require("./adminProcess");
-
-auth.checkAuth = function(req, res, next) {
-  AdminProcess.checkAuth(req, res);
-};
--- server/Admin/adminRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const adminAction = require("./adminAction");
-
-//--Connexion
-//router.post('/login', account.login);
-router.post("/login", adminAction.checkAuth);
-module.exports = router;
